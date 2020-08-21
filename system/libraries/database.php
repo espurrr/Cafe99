8ef610a845605804c617209c4025b5a4abb9f530
JellyAB <buddhini.abay@gmail.com>
2020-08-21 09:08:33 +0530

    included query methods

--- system/libraries/database.php
+++ system/libraries/database.php
@@ -19,5 +19,49 @@ class Database {
 
         }
     }
+      /*
+    Query method will receive all the database queries
+    */
+    public function Query($query, $options=[]){
+        if(empty($options)){    //normal query without where params
+            $this->Query = $this->db->prepare($query);
+            return $this->Query->execute();
+        }else{  //includes where params
+            $this->Query = $this->db->prepare($query);
+            return $this->Query->execute($options);
+        }
+    }
+
+    /*
+    All count method counts the number of rows from the specified table
+    */
+    public function AllCount($table_name){
+
+        //SELECT * fron table_name
+        $this->Query = $this->db->prepare("SELECT * FROM " .$table_name);
+        $this->Query->execute();
+        return $this->Query->rowCount();
+    }
+
+    /*
+    Count method counts the number of rows of a Query
+    */
+    public function Count(){
+        return $this->Query->rowCount();
+    }
+
+    /*
+    All records method fetch all records as objects from the rewuired Normal Query
+    */
+    public function AllRecords(){
+        return $this->Query->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    /*
+    Get a single row of records as object
+    */
+    public function Row(){
+        return $this->Query->fetch(PDO::FETCH_OBJ);
+    }
   
 }
