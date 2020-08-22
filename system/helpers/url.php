fc1d7ad672b3175d8b27aadfc2ee389deb0e28f2
JellyAB <buddhini.abay@gmail.com>
2020-08-22 22:43:46 +0530

    introduce url helper

---
+++ system/helpers/url.php
@@ -0,0 +1,17 @@
+<?php
+
+function link_css( $css_path ){
+    if(!empty($css_path)){
+        return '<link rel="stylesheet" href="'. BASE_URL .'/'. $css_path .'">';
+
+    }
+}
+
+function link_js( $js_path ){
+    if(!empty($js_path)){
+        return '<script src="'. BASE_URL .'/'. $js_path .'"></script>';
+
+    }
+}
+
+?>
