7fd118f40586aa062628ca6f6ac54493d04653f5
JellyAB <buddhini.abay@gmail.com>
2020-08-24 23:34:54 +0530

    new helper html

---
+++ system/helpers/html.php
@@ -0,0 +1,20 @@
+<?php
+
+function anchor($href, $value, $options = []){
+    if(array_key_exists("class", $options)){
+        $class = $options['class'];
+    } else {
+        $class = null;
+    }
+
+    if(array_key_exists("id", $options)){
+        $id = $options['id'];
+    } else {
+        $id = null;
+    }
+    $url = BASE_URL . "/" . $href; 
+    return '<a href="'. $url .'" class="'. $class .'" id="'. $id .'">'. $value .'</a>';
+
+}
+
+?>
