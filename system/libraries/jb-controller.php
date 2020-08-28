1334f2ec1f425a1957fad4b6abdbb49de848c849
JellyAB <buddhini.abay@gmail.com>
2020-08-28 08:32:42 +0530

    mod jbcontroller add post get functions

--- system/libraries/jb-controller.php
+++ system/libraries/jb-controller.php
@@ -1,6 +1,15 @@
 <?php
 
 class JB_Controller {
+
+    public function __construct(){
+        if(file_exists("../system/config/autoload.php")){
+            require_once "../system/config/autoload.php";
+            $helpers = $autoload['helpers'];
+            $this->helper($helpers);
+
+        }
+    }
     /*
     Load view in the controller
     */
@@ -43,6 +52,40 @@ class JB_Controller {
             endforeach;
         }
     }
+
+    /*
+    POST function
+    */ 
+
+    public function post($field_name){
+
+        if($_SERVER['REQUEST_METHOD'] == "post" || $_SERVER['REQUEST_METHOD'] == "POST"){
+              
+          return strip_tags(trim($_POST[$field_name]));
+          //trim removes extra whitespaces from right and left
+          //strip_tags prevents cross-side-scripting attacks
+
+        } else {
+          die("<div style='background-color:#f1f4f4;color:#afaaaa;border: 1px dotted #afaaaa;padding: 10px; border-radius: 4px'>Sorry Method is not POST</div>");
+        }
+
+    }
+
+    /*
+    GET function
+    */ 
+    public function get($field_name){
+
+        if($_SERVER['REQUEST_METHOD'] == "get" || $_SERVER['REQUEST_METHOD'] == "GET"){
+              
+          return strip_tags(trim($_GET[$field_name]));
+
+        } else {
+          die("<div style='background-color:#f1f4f4;color:#afaaaa;border: 1px dotted #afaaaa;padding: 10px; border-radius: 4px'>Sorry Method is not GET</div>");
+        }
+
+    }
+
 
 }
 
