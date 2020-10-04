ac5ad176aacb23ab1865eed052a6001b66364eba
MVIUdani <imeudani@gmail.com>
2020-10-04 12:12:46 +0530

    included sidebar css ref locaton

---
+++ application/views/sidebar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Manager side bar</title>
+    <link rel="stylesheet" href="sidebar.css">
+</head>
+<body>
+    
+    <div class="wrapper">
+        <div class="sidebar">
+            <a class="logo" href="#">
+                <svg></svg>
+            </a>
+            <ul>
+                <li><a href="#">Announcements</a></li>
+                <li><a href="#">Users</a></li>
+                <li><a href="#">Orders</a></li>
+                <li><a href="#">Food Menu</a></li>
+                <li><a href="#">Analytics</a></li>
+            </ul>
+
+        </div>
+    </div>
+</body>
+</html>
