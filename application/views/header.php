7d17e5f89ae9bb823ba2f128f8c0872acdae6189
ChenukaMedawela <chenukas@gmail.com>
2020-10-11 19:40:16 +0530

    header dala thiyenne ban

---
+++ application/views/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- Somehow I got an error, so I comment the title, just uncomment to show -->
+    <!-- <title>Responsive Navigation Menu</title> -->
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <!-- <script type="script.js"></script> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <nav>
+      <div class="logo top"><img src="logo.png"></div>
+<input type="checkbox" id="click">
+      <label for="click" class="menu-btn top">
+        <i class="fas fa-bars"></i>
+      </label>
+      <ul>
+<li><a href="#">Food</a></li>
+<li><a href="#">Drinks</a></li>
+<li><a href="#">Desserts</a></li>
+<li>
+<div class="search-container">
+    <form action="/<!-- action_page.php -->">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit"><i class="fa fa-search"></i></button>
+    </form>
+  </div></li>
+<div id="logsign">
+  <li ><a id="logsign1" href="#">LogIn</a></li>
+  <li ><a id="logsign2" href="#">SignUp</a></li>
+</div>
+</ul>
+</nav>
+
+
+</body>
+</html>
