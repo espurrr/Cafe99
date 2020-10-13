582236b98105bc94fe90c6596842e9fa14dba17d
JellyAB <buddhini.abay@gmail.com>
2020-10-14 00:08:46 +0530

    mod header

--- application/views/header.php
+++ application/views/header.php
@@ -1,18 +1,13 @@
-<!DOCTYPE html>
-<!-- Created By CodingNepal -->
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <!-- Somehow I got an error, so I comment the title, just uncomment to show -->
-    <!-- <title>Responsive Navigation Menu</title> -->
+
+
+<!-- //Include these in the header of the page which the code is included
+
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <!-- <script type="script.js"></script> -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  </head>
-  <body>
-    <nav>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script> 
+  -->
+    
+  <nav>
       <div class="logo top"><img src="logo.png"></div>
 <input type="checkbox" id="click">
       <label for="click" class="menu-btn top">
@@ -25,17 +20,13 @@
 <li>
 <div class="search-container">
     <form action="/<!-- action_page.php -->">
-      <input type="text" placeholder="Search.." name="search">
+      <input class="search" type="text" placeholder="Search.." name="search">
       <button type="submit"><i class="fa fa-search"></i></button>
     </form>
   </div></li>
 <div id="logsign">
-  <li ><a id="logsign1" href="#">LogIn</a></li>
-  <li ><a id="logsign2" href="#">SignUp</a></li>
+  <li ><a id="logsign1" href="#">Login</a></li>
+  <li ><a id="logsign2" href="#">Sign Up</a></li>
 </div>
 </ul>
 </nav>
-
-
-</body>
-</html>
