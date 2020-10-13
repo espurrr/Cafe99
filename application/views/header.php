73323426a05b824d387d253d5180b62fe87f0acd
JellyAB <buddhini.abay@gmail.com>
2020-10-14 03:13:37 +0530

    alert styling

--- application/views/header.php
+++ application/views/header.php
@@ -1,14 +1,6 @@
 
-
-<!-- //Include these in the header of the page which the code is included
-
-    <link rel="stylesheet" href="header.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script> 
-  -->
-    
-  <nav>
-      <div class="logo top"><img src="logo.png"></div>
+<nav>
+      <div class="logo top"><img src="../../public/images/logo.png"></div>
 <input type="checkbox" id="click">
       <label for="click" class="menu-btn top">
         <i class="fas fa-bars"></i>
@@ -19,14 +11,17 @@
 <li><a href="#">Desserts</a></li>
 <li>
 <div class="search-container">
-    <form action="/<!-- action_page.php -->">
-      <input class="search" type="text" placeholder="Search.." name="search">
+    <form action="#">
+      <input class="search" type="text" placeholder="  Search.." name="search">
       <button type="submit"><i class="fa fa-search"></i></button>
     </form>
   </div></li>
 <div id="logsign">
   <li ><a id="logsign1" href="#">Login</a></li>
-  <li ><a id="logsign2" href="#">Sign Up</a></li>
+  <li ><a id="logsign2" href="#">SignUp</a></li>
 </div>
 </ul>
 </nav>
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+
