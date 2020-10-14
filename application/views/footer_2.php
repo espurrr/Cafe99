eedb8d5698045ea0ad720afc318edf601fa9ff31
MVIUdani <imeudani@gmail.com>
2020-10-14 11:42:36 +0530

    footer_2 files add

---
+++ application/views/footer_2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href=".../public/css/footer_2.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>footer2</title>
+    </head>
+    <body>
+      
+    
+<footer class="footer">
+    <div class="container">
+    
+    <div class="footer__empty"></div>
+    
+    <div class="footer__allrightreserved" >
+        &copy;2020Cafe99.All Right Reserved.
+        Powered by Dashcode.
+    </div>
+
+    <div class="footer__image" >
+        <a href="#"><i class="fa fa-cc-visa"></i></a>
+        <a href="#"><i class="fa fa-cc-mastercard"></i></a>
+        <a href="#"> <i class="fa fa-cc-amex"></i></a>
+    </div>
+     
+  </div>
+</footer>
+    </body>
+</html>
