5b8ff586b24991990a449090e8c365896960ff29
JellyAB <buddhini.abay@gmail.com>
2020-10-15 13:41:24 +0530

    mod header

--- application/views/login.php
+++ application/views/login.php
@@ -4,12 +4,14 @@
     <title>Login to Cafe99</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500&display=swap" rel="stylesheet">
     <?php echo link_css("css/signup.css"); ?>
+    <?php echo link_css("css/header.css"); ?>
 	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 </head>
 <body>
-
-<div class="container" id="container">
-	<div class="sign-up-container">
+<?php include 'header.php'?>
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<div class="parent-container" id="p-container">
+      <div class="sign-up-container">
 		<form action="#"><br>
 			<h1>Log In</h1><br>
 		
@@ -17,7 +19,7 @@
             <input type="password" name="password" placeholder="Password*">
             <a href="#">Forgot Password?</a>
             <button>Log in</button>
-            
+           
 		</form>
 	</div>
 </div>
