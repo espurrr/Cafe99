0ed1fc5d328b3866a1fa2687d46dd25dc8c719c9
ChenukaMedawela <chenukas@gmail.com>
2020-11-05 00:41:42 +0530

    Deleted Old header

--- application/views/header.php
+++ application/views/header.php
@@ -1,27 +1,99 @@
+<html>
 
-<nav>
-      <div class="logo top"><img src="../../public/images/logo.png"></div>
-<input type="checkbox" id="click">
-      <label for="click" class="menu-btn top">
-        <i class="fas fa-bars"></i>
-      </label>
-      <ul>
-<li><a href="#">Food</a></li>
-<li><a href="#">Drinks</a></li>
-<li><a href="#">Desserts</a></li>
-<li>
-<div class="search-container">
-    <form action="#">
-      <input class="search" type="text" placeholder="  Search.." name="search">
-      <button type="submit"><i class="fa fa-search"></i></button>
-    </form>
-  </div></li>
-<div id="logsign">
-  <li ><a id="logsign1" href="#">Login</a></li>
-  <li ><a id="logsign2" href="#">SignUp</a></li>
-</div>
-</ul>
-</nav>
-<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="header.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+</head>
 
+<body>
+  <div class="topnav" id="myTopnav">
+   <img class="logo" id="mlogo" src="logo.png">
+    
+    <div class="all-nav" id="nav" >
+      <div class="route-dropdown" onclick="toggleMenu(this)">
+      <button class="dropbtn">Food 
+          <i class="fa fa-caret-down"></i>
+      </button>
+      <div id="waypoints" class="dropdown-content" >
+        <a href="#">Cat1</a>
+        <a href="#">Cat2</a>
+        <a href="#">Cat3</a>
+        <a href="#">Cat4</a>
+        <a href="#">Cat5</a>
+      </div>
+    </div>
+      <div class="route-dropdown" onclick="toggleMenu(this)">
+        <button class="dropbtn">Drink
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div id="routes" class="dropdown-content" >
+            <a href="#">Cat1</a>
+            <a href="#">Cat2</a>
+            <a href="#">Cat3</a>
+            <a href="#">Cat4</a>
+            <a href="#">Cat5</a>
+        </div>
+      </div>
+      <div class="route-dropdown" onclick="toggleMenu(this)">
+        <button class="dropbtn">Desert 
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div id="routes" class="dropdown-content" >
+            <a href="#">Cat1</a>
+            <a href="#">Cat2</a>
+            <a href="#">Cat3</a>
+            <a href="#">Cat4</a>
+            <a href="#">Cat5</a>
+        </div>
+      </div>
+      <div class="route-dropdown" onclick="toggleMenu(this)">
+        <button class="dropbtn">Cart
+        </button>
+      </div>
+        <div class="signlog">
+          <div class="route-dropdown" style="padding-right: 10px;" onclick="toggleMenu(this)">
+        <button class="dropbtn" id="logsign1">LogIn
+        </button>
+      </div>
+        <div class="route-dropdown" style="padding-left: 10px;" onclick="toggleMenu(this)">
+        <button class="dropbtn" id="logsign2">SignUp
+        </button>
+      </div>
+        </div>
+      
+    </div>
+    <a href="javascript:void(0);" style="font-size:15px;border-radius: 0px;" class="icon" onclick="myFunction1(); myFunction2();myFunction3();">&#9776;</a>
+  </div>
+
+<script type="text/javascript">
+      function myFunction1() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+      function myFunction2() {
+    var y = document.getElementById("nav");
+    if (y.className === "all-nav") {
+      y.className += " responsive";
+    } else {
+      y.className = "all-nav";
+    }
+  }
+   function myFunction3() {
+    var z = document.getElementById("mlogo");
+    if (z.className === "logo") {
+      z.className += " responsive";
+    } else {
+      z.className = "logo";
+    }
+  }
+</script>
+</body>
+
+</html>
 
