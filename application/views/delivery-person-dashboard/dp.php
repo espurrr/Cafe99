dd6a8897329cfeb37e16cd8a25d5fb462a35aea2
MVIUdani <imeudani@gmail.com>
2020-11-10 11:53:52 +0530

    delivery-person-dashboard add

---
+++ application/views/delivery-person-dashboard/dp.php
@@ -0,0 +1,145 @@
+<html>
+<title>Delivery Person Dashboard</title>
+<head> 
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="newsfeed.css">
+  <link rel="stylesheet" type="text/css" href="dpstyle.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+  #more {display: none;}
+  #more1 {display: none;}
+  </style>
+ </head>
+
+   
+<body>
+ 
+  <input type="checkbox" id="menu">
+
+  <nav>
+  
+  <label for="menu" class="menu-bar">
+           
+  <i class="fa fa-bars"></i>
+           
+  </label>
+
+  </nav>
+
+
+  <div class="sidebar">
+  <div class="profile-details">
+             <div class="avatar"><i class="fa fa-user-circle"></i></div>
+             <div class="profile-text">
+                <p class="name">Kavinda Dias</p>
+                <p class="role">Delivery Person</p>
+            </div>
+        </div>
+            <ul>
+                <li><a href="neworders.php">Orders</a></li>
+                <li><a class="active" href="dp.php">News Feed</a></li>
+                
+                <li><a href="#">LogOut</a></li>
+                
+            </ul>
+
+  </div>
+  <div class="newsfeed-wrapper">
+  <div  class="Dp-content">
+  <div class="content">
+      <h2 class="page-title">News</h2>
+      <div class="dashboard" id="download">
+                    <div class="post">
+                    <div class="top">
+                            <div class="img">
+                            <i class="fa fa-user-circle" aria-hidden="true" style="font-size:60px"></i>
+                            </div>
+                            <div class="name">
+                                <strong><a href="#"><span class="text-name">Kamal Perera</span></a></strong>
+                                <div class="date">
+                                    <span class="text-when">2020/10/23 at 4:00pm</span> ·<img src="http://social-prank.foxsash.com/assets/images/facebook/icon_public.jpg" width="16" height="16" id="visiblefor-icon">
+                                </div>
+                            </div>
+                    </div>
+                            <div class="employee"><i class="fa fa-users"></i>  &nbsp;All Employees</div>
+                            </div>
+
+                            <div class="news_content" style="padding-left:20px;padding-right:20px">
+                            <div class="text_title"><p>Title of the annoucement</p></div>
+                            <br>
+                            <div class="text-message"><p>The nation’s most beloved fast-casual Italian franchise, Squisito Pizza & Pasta, is inviting<span id="dots">...</span><span id="more"> the community to celebrate National Pasta Month this October. With 10 locations including three scheduled to debut this fall, Squisito continues to serve up the perfect recipe for unwavering success and longevity. Despite the ongoing pandemic, Squisito remains committed to the communities in which it does business. During quarantine, the restaurant added family meal deals and access to grocery items and other provisions to offer added ease of convenience to its customers. Squisito also donated thousands of dollars to medical facilities through its catering match program as well as extended further support with complimentary meals to our healthcare heroes.</span></p></div>
+                            <button onclick="myFunction()" id="myBtn">Read more</button>
+                           </div>
+                    </div>
+
+
+                    <div class="dashboard" id="download">
+                    <div class="post">
+                    <div class="top">
+                            <div class="img">
+                            <i class="fa fa-user-circle" aria-hidden="true" style="font-size:60px"></i>
+                            </div>
+                            <div class="name">
+                                <strong><a href="#"><span class="text-name">Kamal Perera</span></a></strong>
+                                <div class="date">
+                                    <span class="text-when">2020/10/15 at 9:00am</span> ·<img src="http://social-prank.foxsash.com/assets/images/facebook/icon_public.jpg" width="16" height="16" id="visiblefor-icon">
+                                </div>
+                            </div>
+                    </div>
+                            <div class="employee"><i class="fa fa-users"></i>  &nbsp;All Employees</div>
+                            </div>
+
+                            <div class="news_content" style="padding-left:20px;padding-right:20px">
+                            <div class="text_title"><p>Title of the annoucement</p></div>
+                            <br>
+                            <div class="text-message"><p>The number of COVID-19 cases in Sri Lanka is on the rise again, and it feels a lot heavier than the first wave. A new cluster emerged a few days back, and right now, there are 1514 active cases<span id="dots1">...</span><span id="more1"> in the country (13/10/2020, 10.54 AM). 
+ 
+                            However, unlike the first time, many of the restaurants, hotels, supermarkets and other retail stores are still functioning, which is a good thing. But, it's absolutely vital to do it in a manner that it protects the employees, customers, and communities.</span></p></div>
+                            <button onclick="myFunction1()" id="myBtn1">Read more</button>
+                           </div>
+                    </div>
+
+     
+</div>
+
+<script>
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction1() {
+  var dots1 = document.getElementById("dots1");
+  var moreText1 = document.getElementById("more1");
+  var btnText1 = document.getElementById("myBtn1");
+
+  if (dots1.style.display === "none") {
+    dots1.style.display = "inline";
+    btnText1.innerHTML = "Read more"; 
+    moreText1.style.display = "none";
+  } else {
+    dots1.style.display = "none";
+    btnText1.innerHTML = "Read less"; 
+    moreText1.style.display = "inline";
+  }
+}
+
+</script>
+
+</body>
+
+</html>
+  
+  
---
+++ application/views/delivery-person-dashboard/ondelivery.php
@@ -0,0 +1,91 @@
+<html>
+<title>Delivery Person Dashboard</title>
+<head> 
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="dpstyle.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+ </head>
+
+   
+<body>
+ 
+  <input type="checkbox" id="menu">
+
+  <nav>
+  
+  <label for="menu" class="menu-bar">
+           
+  <i class="fa fa-bars"></i>
+           
+  </label>
+
+  </nav>
+
+
+  <div class="sidebar">
+  <div class="profile-details">
+             <div class="avatar"><i class="fa fa-user-circle"></i></div>
+             <div class="profile-text">
+                <p class="name">Kavinda Dias</p>
+                <p class="role">Delivery Person</p>
+            </div>
+        </div>
+            <ul>
+                <li><a class="active" href="./dporders.php">Orders</a></li>
+                <li><a  href="./dp.php">News Feed</a></li>
+                
+                <li><a href="#">LogOut</a></li>
+                
+            </ul>
+
+  </div>
+
+  <div  class="Dp-content">
+  <div class="icons">
+  <a href="neworders.php" button class="btn">New<i class='fas fa-clipboard-list' style="font-size:24px;padding-right:40px"></i></button> </a>
+ 
+  <a  class="active" href="ondelivery.php" button class="btn"><i class='fas fa-biking' style="font-size:24px;padding-right:40px"></i></button> </a>
+ 
+  <a href="dispatched.php" button class="btn"><i class='fas fa-clipboard-check' style="font-size:24px"></i></button> </a>
+  </div>
+
+  <div class="list">
+  <ul>
+    <li>Order No</li>
+    <li>Customer Name</li>
+    <li>Customer Address</li>
+    <li>Action</li>
+  </ul>
+  <ul>
+    <li data-label="order no">1</li>
+    <li data-label="customer name"><p>Ishan Senanayaka</p></li>
+    <li data-label="customer address"><p>No.25,<br>New Rd,<br>Maharagama.</p></li>
+    <li data-label="action"><a href="#"><i class="fa fa-check-square" style="font-size:24px;color: #3ace67ee"></i></a></li>
+  </ul>
+
+  <ul>
+    <li data-label="order no">2</li>
+    <li data-label="customer name"><p>Vageesha Perera</p></li>
+    <li data-label="customer address"><p>No.200,<br>Highlevel Rd,<br>Maharagama.</p></li>
+    <li data-label="action"><a href="#"><i class="fa fa-check-square" style="font-size:24px;color: #3ace67ee"></i></a></li>
+  </ul>
+
+  <ul>
+    <li data-label="order no">3</li>
+    <li data-label="customer name"><p>Nilu Vishaka</p></li>
+    <li data-label="customer address"><p>No.45,<br>Pamunuwa Rd,<br>Maharagama.</p></li>
+    <li data-label="action"><a href="#"><i class="fa fa-check-square" style="font-size:24px;color: #3ace67ee"></i></a></li>
+  </ul>
+
+</div>
+
+  
+</div>
+
+</body>
+
+</html>
+  
+  
