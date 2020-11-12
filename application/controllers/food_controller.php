01496ccc6efc816bfe98eb4a55e336c77e561a4b
MVIUdani <imeudani@gmail.com>
2020-11-12 13:17:56 +0530

    merge effect

--- application/controllers/food_controller.php
+++ application/controllers/food_controller.php
@@ -2,16 +2,24 @@
 class Food_Controller extends JB_Controller{
 
     public function index(){
+        $this->view('food-item'); 
+    }
+ //   public function menu(){
+   //     $this->view('food-menu');
+    //}
+   
+    public function menu($cat="",$subcat="",$id=""){
+        if(empty($cat) AND empty($subcat) AND empty($id)){
+            $this->view('food-menu');
+        }else if(!empty($cat) AND empty($subcat) AND empty($id)){
+            $this->view('food-menu/$cat');
+        }else if(!empty($cat) AND !empty($subcat) AND empty($id)){
+            $this->view('food-menu/$cat/$subcat');
+        }else if(!empty($cat) AND !empty($subcat) AND !empty($id)){
+            $this->view('food-menu/$cat/$subcat/$id');
+        }
         
     }
-    public function menu(){
-        $this->view('food-menu');
-    }
-    public function test(){
-        $this->view('deliveryperson/dp');
-    }
     
-    
-
 }
 ?>
