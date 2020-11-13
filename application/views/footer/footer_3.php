f59274425caf8cec4149aef1bd2ee0ea0fd5f6c3
yeshanGunawardana <64021514+yeshanGune@users.noreply.github.com>
2020-11-13 13:51:41 +0530

    footer_3

---
+++ application/views/footer/footer_3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer_3.css?ts=<?=time()?>">
+    <title>Document</title>
+</head>
+<body>
+<footer>
+    <div class="footer-container">
+        <div class="image" style="background-image: url('img/logo.png');"></div>
+        <div class="text"><p>Powered by Team DashCode</p></div>
+    </div>
+</footer>
+
+</body>
+</html>
