a3de9d9bbfdc70b5cb1ca9562c71eb578415656a
yeshanGunawardana <64021514+yeshanGune@users.noreply.github.com>
2020-11-30 00:23:23 +0530

    header css

--- application/views/header/header.php
+++ application/views/header/header.php
@@ -20,39 +20,38 @@
     
    <a href="<?php echo BASE_URL?>/account_controller/index" class="logo-link"><img class="logo" id="mlogo" src="<?php echo BASE_URL?>/public/images/logo.png"></a>
     
-    <div class="all-nav" id="nav" >
+    <div class="all-nav" id="nav" style="background: #FAD74E url('<?php echo BASE_URL?>/public/images/home/texture.png') repeat;">
       <div class="route-dropdown" onclick="toggleMenu(this)">
           <button class="dropbtn">Food<i class="fa fa-caret-down"></i></button>
           <div id="waypoints" class="dropdown-content" >
-      <?php echo anchor("food_controller/menu/food/rice", "Rice") ?>
-      <?php echo anchor("food_controller/menu/food/pizza", "Pizza") ?>
-      <?php echo anchor("food_controller/menu/food/savouries", "Savouries") ?>
-      <?php echo anchor("food_controller/menu/food/cakes", "Cakes") ?>
-      <?php echo anchor("food_controller/menu/food/noodlespastas", "Noodles & Pasta") ?>
-      <?php echo anchor("food_controller/menu/food/biriyani", "Biriyani") ?>
-      <?php echo anchor("food_controller/menu/food/buns", "Buns") ?>
-      </div>
+            <?php echo anchor("food_controller/menu/food/rice", "Rice") ?>
+            <?php echo anchor("food_controller/menu/food/pizza", "Pizza") ?>
+            <?php echo anchor("food_controller/menu/food/savouries", "Savouries") ?>
+            <?php echo anchor("food_controller/menu/food/cakes", "Cakes") ?>
+            <?php echo anchor("food_controller/menu/food/noodlespastas", "Noodles & Pasta") ?>
+            <?php echo anchor("food_controller/menu/food/biriyani", "Biriyani") ?>
+            <?php echo anchor("food_controller/menu/food/buns", "Buns") ?>
+          </div>
       </div>
 
       <div class="route-dropdown" onclick="toggleMenu(this)">
           <button class="dropbtn">Drinks<i class="fa fa-caret-down"></i></button>
           <div id="routes" class="dropdown-content" >
-        <?php echo anchor("food_controller/menu/drinks/tea", "Tea") ?>
-        <?php echo anchor("food_controller/menu/drinks/milkshakes", "Milkshakes") ?>
-        <?php echo anchor("food_controller/menu/drinks/iceblended", "Ice Blended") ?>
-        <?php echo anchor("food_controller/menu/drinks/freshfruitjuice", "Fresh Fruit Juice") ?>
-        <?php echo anchor("food_controller/menu/drinks/coffee", "Coffee") ?>
-        </div>
+            <?php echo anchor("food_controller/menu/drinks/tea", "Tea") ?>
+            <?php echo anchor("food_controller/menu/drinks/milkshakes", "Milkshakes") ?>
+            <?php echo anchor("food_controller/menu/drinks/iceblended", "Ice Blended") ?>
+            <?php echo anchor("food_controller/menu/drinks/freshfruitjuice", "Fresh Fruit Juice") ?>
+            <?php echo anchor("food_controller/menu/drinks/coffee", "Coffee") ?>
+          </div>
       </div>
 
       <div class="route-dropdown" onclick="toggleMenu(this)">
         <button class="dropbtn">Desserts<i class="fa fa-caret-down"></i></button>
         <div id="routes" class="dropdown-content" >
-        <?php echo anchor("food_controller/menu/desserts/icecreams", "Ice creams") ?>
-        <?php echo anchor("food_controller/menu/desserts/custardsandpuddings", "Custards & Puddings") ?>
+            <?php echo anchor("food_controller/menu/desserts/icecreams", "Ice creams") ?>
+            <?php echo anchor("food_controller/menu/desserts/custardsandpuddings", "Custards & Puddings") ?>
             <?php echo anchor("food_controller/menu/desserts/muffins", "Muffins") ?>
             <?php echo anchor("food_controller/menu/desserts/cheesecakes", "Cheesecakes") ?>
-       
         </div>
       </div>
       
