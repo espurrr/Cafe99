f64073c975cb56548a066b8dbd99268f8a6c1fc2
yeshanGunawardana <64021514+yeshanGune@users.noreply.github.com>
2020-11-30 23:47:31 +0530

    km foodmenu

--- application/views/kitchenmanager/foodmenu/foodmenu.php
+++ application/views/kitchenmanager/foodmenu/foodmenu.php
@@ -433,7 +433,7 @@
         <main class="grid">
 
         <?php foreach($data as $row): ?>
-        <?php if($row->Category_name === "Desserts" && $row->Subcategory_name === "Icecreams"):?>
+        <?php if($row->Category_name === "Desserts" && $row->Subcategory_name === "IceCreams"):?>
                 <article>
                     <?php
                         $img_path = BASE_URL."/public/images/food-dash-images/".$row->Category_name."/".$row->Subcategory_name."/".str_replace(' ','',$row->Food_name).".jpg";
