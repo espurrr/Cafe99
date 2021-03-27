c8dcf3b81df88defe21bc522a03b68f4f56be489
MVIUdani <imeudani@gmail.com>
2021-03-27 19:07:01 +0530

    create user block modal

---
+++ public/js/restaurantmanager/user_block.js
@@ -0,0 +1,29 @@
+var block_popup_win = document.getElementById("block-popup-window");
+var block_modal_cancel_btn = document.getElementById("block-modal-Bcancel-btn");
+var block_modal_block_btn = document.getElementById("block-modal-blocked-btn");
+
+function showBlockModal(id) {
+    var id = id;
+    $("#block-modal-blocked-btn").data("id", id); //sets data id attribute
+    block_popup_win.style.display = "block";
+    document.getElementById("block-message").innerHTML =
+        "Are you sure you want to block this user?";
+        
+}
+
+block_modal_cancel_btn.onclick = function() {
+    block_popup_win.style.display = "none";
+};
+$("#block-modal-blocked-btn").click(function() {
+    var id = $(this).data("id");
+    // alert(id);
+    $.ajax({
+        url: "http://localhost/cafe99/rm_controller/block_user/" + id,
+
+        success: function() {
+            block_popup_win.style.display = "none";
+            location.reload(true);
+           
+        },
+    });
+});
